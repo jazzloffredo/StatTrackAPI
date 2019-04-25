03bfbf2022a0fa96d4a83ade83111ae93b7b554d
Jazz Loffredo <jazzloffredo@Jazzs-MBP-2.attlocal.net>
2019-04-24 22:30:23 -0500

    Team service functioning to retrieve all teams

--- src/team/interfaces/team.interface.ts
+++ src/team/interfaces/team.interface.ts
@@ -2,4 +2,10 @@ export interface Team {
     teamID: string;
     teamName: string;
     isActive: string;
+    totalGames: number;
+    totalWins: number;
+    totalLosses: number;
+    winLossRatio: number;
+    leagueWins: number;
+    worldSeriesWins: number;
 }
--- src/team/team.service.ts
+++ src/team/team.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConnectionPool } from 'mssql';
 
-import { ConfigurationService } from 'src/configuration/configuration.service';
+import { ConfigurationService } from '../configuration/configuration.service';
 import { Team } from './interfaces/team.interface';
 
 @Injectable()
@@ -13,7 +13,7 @@ export class TeamService {
 
         const pool = new ConnectionPool(this.configService.dbConfig);
 
-        let teams: Team[];
+        const teams: Team[] = [];
 
         try {
             await pool.connect();
@@ -21,6 +21,21 @@ export class TeamService {
             const result = await pool.request().execute('Stats.ListTeams');
 
             const resultAsTable = result.recordset.toTable();
+
+            for (const curRow of resultAsTable.rows) {
+                const curTeam: Team = {
+                    teamID: curRow[0],
+                    teamName: curRow[1],
+                    isActive: curRow[2],
+                    totalGames: curRow[3],
+                    totalWins: curRow[4],
+                    totalLosses: curRow[5],
+                    winLossRatio: curRow[6],
+                    leagueWins: curRow[7],
+                    worldSeriesWins: curRow[8],
+                };
+                teams.push(curTeam);
+            }
 
         } catch (err) {
             Logger.log(err);
