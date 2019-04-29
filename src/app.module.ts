975f516d2c90e429e2f9aa6b22cd697d0cc9620d
Jazz Loffredo <jloffredo@users.campus>
2019-04-29 15:34:00 -0500

    Added learderboard controller and service to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,12 @@ import { TeamController } from './team/team.controller';
 import { TeamService } from './team/team.service';
 import { PlayerService } from './player/player.service';
 import { PlayerController } from './player/player.controller';
+import { LeaderboardController } from './leaderboard/leaderboard.controller';
+import { LeaderboardService } from 'dist/leaderboard/leaderboard.service';
 
 @Module({
   imports: [ConfigurationModule],
-  controllers: [AppController, AuthController, TeamController, PlayerController],
-  providers: [AppService, AuthService, TeamService, PlayerService],
+  controllers: [AppController, AuthController, TeamController, PlayerController, LeaderboardController],
+  providers: [AppService, AuthService, TeamService, PlayerService, LeaderboardService],
 })
 export class AppModule {}
