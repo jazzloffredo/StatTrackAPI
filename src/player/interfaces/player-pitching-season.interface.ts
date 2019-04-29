24ebf6f4f11b33ba1aaec2138d609fe6838a4e6b
Jazz Loffredo <jazzloffredo@ksu-wireless-187-187.campus.ksu.edu>
2019-04-29 08:20:20 -0500

    Merged in feature/player, added player season functionality

---
+++ src/player/interfaces/player-pitching-season.interface.ts
@@ -0,0 +1,17 @@
+export interface PlayerPitchingSeason {
+    year: number;
+    teamName: string;
+    wins: number;
+    losses: number;
+    games: string;
+    completeGames: number;
+    shutouts: number;
+    saves: number;
+    hitsAllowed: number;
+    homerunsAllowed: number;
+    earnedRuns: number;
+    ERA: number;
+    walks: number;
+    strikeouts: number;
+    opponentAverage: number;
+}
--- src/player/player.controller.ts
+++ src/player/player.controller.ts
@@ -3,6 +3,9 @@ import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { PlayerService } from './player.service';
 import { Player } from './interfaces/player.interface';
 import { UserFavoritePlayerDTO } from './dto/user-favorite-player.dto';
+import { PlayerBattingSeason } from './interfaces/player-batting-season.interface';
+import { PlayerFieldingSeason } from './interfaces/player-fielding-season.inteface';
+import { PlayerPitchingSeason } from './interfaces/player-pitching-season.interface';
 
 @Controller('player')
 export class PlayerController {
@@ -14,9 +17,29 @@ export class PlayerController {
         return await this.playerService.retrieveAllPlayersGivenChar(params.lastNameChar);
     }
 
+    @Get('retrievePlayerBattingSeasons/:playerID')
+    async retrievePlayerBattingSeasons(@Param() params): Promise<PlayerBattingSeason[]> {
+        return await this.playerService.retrievePlayerBattingSeasons(params.playerID);
+    }
+
+    @Get('retrievePlayerFieldingSeasons/:playerID')
+    async retrievePlayerFieldingSeasons(@Param() params): Promise<PlayerFieldingSeason[]> {
+        return await this.playerService.retrievePlayerFieldingSeasons(params.playerID);
+    }
+
+    @Get('retrievePlayerPitchingSeasons/:playerID')
+    async retrievePlayerPitchingSeasons(@Param() params): Promise<PlayerPitchingSeason[]> {
+        return await this.playerService.retrievePlayerPitchingSeasons(params.playerID);
+    }
+
     @Get('retrieveFavoritePlayers/:username')
     async retrieveFavorteTeamsForUser(@Param() params): Promise<string[]> {
         return await this.playerService.retrieveFavoritePlayersForUser(params.username);
+    }
+
+    @Get('mapPlayerIdToName/:playerID')
+    async mapPlayerIdToName(@Param() params): Promise<string[]> {
+        return await this.playerService.mapPlayerIdToName(params.playerID);
     }
 
     @Post('addFavoritePlayerForUser')
