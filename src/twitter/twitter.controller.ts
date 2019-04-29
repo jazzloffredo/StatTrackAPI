0a8f6056aa3ccd93bfd0ecfa911735615a49143d
Jazz Loffredo <jloffredo@users.campus>
2019-04-29 16:22:44 -0500

    Twitter built

--- src/twitter/twitter.controller.ts
+++ src/twitter/twitter.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 
 import { TwitterService } from './twitter.service';
+import { Tweet } from 'dist/twitter/interfaces/tweet.interface';
 
 @Controller('twitter')
 export class TwitterController {
@@ -8,7 +9,7 @@ export class TwitterController {
     constructor(private readonly twitterService: TwitterService) { }
 
     @Get('test')
-    async testTwitterAuth() {
-        this.twitterService.test();
+    async retrieveTweetsForName(name: string): Promise<Tweet[]> {
+        return this.twitterService.retrieveTweetsForName(name);
     }
 }
